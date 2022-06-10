05389a903fef81edaa354b309fdd5c90c5302007
lms0016 <lms001616@gmail.com>
2022-06-10 16:33:22 +0800

    Added ignore specific users

--- d2l.py
+++ d2l.py
@@ -12,6 +12,7 @@ load_dotenv()
 # Discord Bot
 discord_bot_id = int(os.environ['DISCORD_BOT_ID'])
 discord_bot_token = os.environ['DISCORD_BOT_TOKEN']
+ignore_discord_uid = os.environ.get('IGNORE_DISCORD_UID', None) # (Optional) Ignore specific discord user
 
 # Discord channel to line
 message_channel_id = os.environ.get('MESSAGE_CHANNEL_ID', None) # (Optional) To monitor a specific channel can be set optional
@@ -36,7 +37,13 @@ set_message_channel = message_channel_id is not None
 
 @bot.listen()
 async def on_message(message):
+
     if message.webhook_id == discord_bot_id: return # Ignore the bot's own messages
+
+    ignore_discord_uid_list = ignore_discord_uid.split(':')
+    # for i in ignore_discord_uid: # Ignore specific user messages
+    if str(message.webhook_id) in ignore_discord_uid_list: return
+
     if set_message_channel is False:
         lotify_message = message.author.display_name + "\n"
         lotify_message += message.content
@@ -45,9 +52,9 @@ async def on_message(message):
             message=lotify_message
         )
     else:
-        count = len(message_channel_list)
+        count = len(message_channel_id_list)
         for i in range(0,count):
-            if message.channel.id == int(message_channel_list[i]):
+            if message.channel.id == int(message_channel_id_list[i]):
                 lotify_message = message.author.display_name + "\n"
                 lotify_message += message.content
                 lotify.send_message(
@@ -66,9 +73,9 @@ if __name__ == "__main__":
             bot.run(discord_bot_token)
     else :
         # Check if the number of elements of lotify_token is the same as the number of elements of message_channel_id
-        message_channel_list = message_channel_id.split(':')
+        message_channel_id_list = message_channel_id.split(':')
         lotify_token_list = lotify_token.split(':')
-        if len(message_channel_list) != len(lotify_token_list):
+        if len(message_channel_id_list) != len(lotify_token_list):
             print("Please make sure the number of MESSAGE_CHANNEL_ID and LOTIFY_TOKEN are the same")
         else:
             bot.run(discord_bot_token)
